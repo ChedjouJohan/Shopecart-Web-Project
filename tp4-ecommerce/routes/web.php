d4c19a8ef0f8f3116f329ef1262a60c02282b0b3
Nguetchuissi B <landrybrunel5@gmail.com>
2025-11-24 07:11:29 +0100

    articles ajout

--- tp4-ecommerce/routes/web.php
+++ tp4-ecommerce/routes/web.php
@@ -24,3 +24,23 @@ Route::get('/', function () {
 Route::get('/Blog.html', function () {
     return view('blog');;
 });
+
+Route::get('/articles/article1.html', function () {
+    return view('articles.article1');
+});
+
+Route::get('/articles/article2.html', function () {
+    return view('articles.article2');
+});
+Route::get('/articles/article3.html', function () {
+    return view('articles.article3');
+});
+Route::get('/articles/article4.html', function () {
+    return view('articles.article4');
+});
+Route::get('/articles/article5.html', function () {
+    return view('articles.article5');
+});
+Route::get('/articles/article6.html', function () {
+    return view('articles.article6');
+});
